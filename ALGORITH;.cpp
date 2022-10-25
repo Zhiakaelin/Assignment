3c1929a540d200893665d6e51bd79cbb08ca5e98
Zhiakaelin <116373035+Zhiakaelin@users.noreply.github.com>
2022-10-25 17:35:05 +0100

    Add files via upload

---
+++ ALGORITH;.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d;
+	printf("enter a number A\n",a);
+	scanf("%d",&a);
+	printf("enter a number B\n",b);
+	scanf("%d",&b);
+	printf("enter a number C\n",c);
+	scanf("%d",&c);
+	printf("Number a=%d , b=%d and c=%d",c,a,b);
+	return 0;	
+}
---
+++ Ex2.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+		int a,b,c,d,e,f;
+	printf("enter a number A\n",a);
+	scanf("%d",&a);
+	printf("enter a number B\n",b);
+	scanf("%d",&b);
+	printf("enter a number C\n",c);
+	scanf("%d",&c);
+	printf("enter a number D\n",c);
+	scanf("%d",&d);
+	printf("enter a number E\n",e);
+	scanf("%d",&e);
+   f=(a+b+c+d+e)/5;
+	printf("the average of the number you enter is= %d\n",f);
+	return 0;
+}
